9a3f3e0a9240059215d95d9f280d2940d1281abe
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2019-12-24 18:00:15 -0600

    hotfix: patched pi-kiosk script

--- pi-kiosk.sh
+++ pi-kiosk.sh
@@ -32,30 +32,39 @@ sed -i '1s/^/disable_overscan=1\n/' /boot/config.txt
 
 echo "Configuring Autologin"
 mkdir -pv /etc/systemd/system/getty@tty1.service.d
-cat <<EOF1 >/etc/systemd/system/getty@tty1.service.d/autologin.conf
+cat <<EOF >/etc/systemd/system/getty@tty1.service.d/autologin.conf
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin pi --noclear %I $TERM
-EOF1
-cat <<EOF2 >/etc/systemd/system/getty@tty1.service.d/noclear.conf
+EOF
+cat <<EOF >/etc/systemd/system/getty@tty1.service.d/noclear.conf
 [Service]
 TTYVTDisallocate=no
-EOF2
+EOF
 
 echo "Configuring Locale and Timezone"
+
+cat <<EOF > /etc/default/keyboard
+XKBMODEL="pc105"
+XKBLAYOUT="us"
+XKBVARIANT=""
+XKBOPTIONS=""
+
+BACKSPACE="guess"
+EOF
+
 timedatectl set-ntp true
 timedatectl set-timezone America/Chicago
 cp /etc/locale.gen /etc/locale.gen.bak
 echo "en_US.UTF-8 UTF-8" | tee /etc/locale.gen
-locale-gen
+locale-gen en_US.UTF-8
 update-locale en_US.UTF-8
-dpkg-reconfigure locales
 
 echo "Install X11, Openbox, Chromium"
 apt install --no-install-recommends -y chromium-browser xserver-xorg x11-xserver-utils xinit openbox
 
 echo "Configuring Openbox"
-cat <<EOF3 >/etc/xdg/openbox/autostart
+cat <<EOF >/etc/xdg/openbox/autostart
 # Disable any form of screen saver / screen blanking / power management
 xset s off
 xset s noblank
@@ -69,6 +78,8 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Loc
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 xrandr --output HDMI-1 --rotate $ORIENTATION
 chromium-browser --disable-infobars --kiosk $WEBSITE
-EOF3
+EOF
 
-echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor" | tee -a /home/pi/.profile
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor' | tee -a /home/pi/.profile
+
+reboot
